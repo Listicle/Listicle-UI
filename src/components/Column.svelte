dbd64484b63cdbf0e9b376985f89f1470fc71922
Carlos Gomez <carlosalbertogomez108@gmail.com>
2021-12-07 21:32:08 -0700

    Add logic to handle the creation of new activities and new tasks

--- src/components/Column.svelte
+++ src/components/Column.svelte
@@ -1,9 +1,28 @@
 <script>
   export let state;
+  import Card from "./Card.svelte";
+  let isCardOn = false;
+  let activities = [];
+
+  const createActivity = () => {
+    isCardOn = true;
+  }
 </script>
 
 <div class="w-1/4 p-4 m-4 rounded">
   <h1 class="text-center text-xl">{state}</h1>
+  <div class="flex flex-row">
+    <p>Add an activity</p>
+    <p on:click={createActivity}>+</p>
+  </div>
+  {#if isCardOn}
+    <Card />
+  {/if}
+  {#if activities.length}
+    {#each activities as activity (createActivity.id)}
+      <Card {activity} {state} />
+    {/each }
+  {/if}
 </div>
 
 <style>
