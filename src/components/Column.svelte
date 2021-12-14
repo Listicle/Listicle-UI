f8e0983d11c927b36322da31cab2dcef33772019
PaulTimothyChambers <63275905+PaulTimothyChambers@users.noreply.github.com>
2021-12-13 23:42:11 -0700

    Refactor to render server-provided props

--- src/components/Column.svelte
+++ src/components/Column.svelte
@@ -1,40 +1,44 @@
 <script>
   import Card from "./Card.svelte";
+
   export let missions;
   export let state;
 
   let missionStatus;
   let missionTitle = '';
-  $:filteredMissions = missions.filter(mission => mission.status === state)
 
-  if(state === 0) {
+  $:filteredMissions = missions.filter(mission => mission.status === state);
+
+  if(state === 'to_do') {
     missionStatus = 'Future Missions'
-  }else if(state === 1) {
+  } else if(state === 'doing') {
     missionStatus = 'Current Missions'
   } else {
     missionStatus = 'Completed Missions'
   }
-  
+
 </script>
 
 <div class="w-72 p-4 m-4 rounded" >
   <h1 class="text-center text-xl font-semibold">{missionStatus}</h1>
   <div class="flex flex-row justify-center">
-    <form >
-      <input class="w-full text-center border-solid border-transparent 
-      focus:outline-none focus:shadow rounded" type="text" placeholder="add a mission" 
-      onfocus="this.placeholder=''" 
-      onblur="this.placeholder='add a mission'"
-      bind:value={missionTitle}>
+    <form>
+      <input
+        class="w-full text-center border-solid border-transparent
+          focus:outline-none focus:shadow rounded"
+        type="text"
+        placeholder="add a mission"
+        onfocus="this.placeholder=''"
+        onblur="this.placeholder='add a mission'"
+        bind:value={missionTitle}
+      >
     </form>
   </div>
-  {#if filteredMissions.length}
     {#each filteredMissions as mission (mission.id)}
       <div>
         <Card {mission} cardState={state} tasks={mission.tasks} />
       </div>
     {/each}
-  {/if}
 </div>
 
 <style>
