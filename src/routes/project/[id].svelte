c103960b747cff045136b5ae80f4e06e98ba36cf
Carlos Gomez <carlosalbertogomez108@gmail.com>
2021-12-15 18:16:43 -0700

    Add responsive design styling

--- src/routes/project/[id].svelte
+++ src/routes/project/[id].svelte
@@ -39,7 +39,7 @@
 
 <div class="w-4/5 h-full min-h-screen">
   <h1 class="text-center text-6xl pt-6 font-semibold">{mission.projectName}</h1>
-  <div class="inner-div flex flex-row py-4 justify-center" >
+  <div class="inner-div flex flex-col md:flex-row py-4 justify-center" >
     {#each status as state}
       <Column {state} missions={mission.activities} projectId={mission.id} />
     {/each}
