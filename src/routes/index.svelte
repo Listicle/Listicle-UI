ccad88398067d9a7a2d42926e8db5a3d88e0c3a0
Carlos Gomez <carlosalbertogomez108@gmail.com>
2021-12-15 18:49:36 -0700

    Update package.json config

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,7 +6,7 @@
   <div class="w-4/5 flex flex-col justify-center items-center space-y-20 backdrop-contrast-200 rounded-md p-6 py-10" >
     <h1 class="text-2xl md:text-5xl">Welcome to Listicle</h1>
     <h2 class="text-xl md:text-3xl">We can help you rocket your productivity</h2>
-    <h3 class="text-xl md:text-3xl">To get started open an existing project on your dashboard</h3>
+    <h3 class="text-xl md:text-3xl">To get started open an existing project</h3>
     <h4 class="text-xl md:text-3xl">Or create a new one by typing a title...</h4>
   </div>
 </main>
